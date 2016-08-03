e65affa7b2bf98898c450afce66dd63215d78c1f
krzysztofkolek <krzysztof.kolek.praca@gmail.com>
2016-08-03 20:13:17 +0200

    Retailers + Added server logic + View

--- client/app/retailers/retailers.controller.js
+++ client/app/retailers/retailers.controller.js
@@ -1,18 +1,35 @@
 'use strict';
 
-(function(){
+(function () {
 
-class RetailersComponent {
-  constructor() {
-    this.message = 'Hello';
-  }
-}
+    class RetailersComponent {
+        constructor($http, $scope, socket) {
+            this.apiUrl = '/api/retailerss/';
+            
+            this.$http = $http;
+            this.$scope = $scope;
+            
+            this.retailers = [];
+            
+            this.$scope.$on('$destroy', function() {
+                socket.unsyncUpdates('retailers');
+            })
+        }
+        
+        $onInit() {
+            this.$http.get(this.apiUrl)
+                .then(response => {
+                   this.retailers = response.data;
+                   this.socket.syncUpdates(this.apiUrl, this.retailers);                    
+                });
+        }
+    }
 
-angular.module('companyFoodDeliveryApp')
-  .component('retailers', {
-    templateUrl: 'app/retailers/retailers.html',
-    controller: RetailersComponent,
-    controllerAs: 'retailersCtrl'
-  });
+    angular.module('companyFoodDeliveryApp')
+        .component('retailers', {
+            templateUrl: 'app/retailers/retailers.html',
+            controller: RetailersComponent,
+            controllerAs: 'retailersCtrl'
+        });
 
 })();
