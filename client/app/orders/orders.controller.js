cbe0653a18d8f977eb1f7f9852e0782937be6e82
krzysztofkolek <krzysztof.kolek.praca@gmail.com>
2016-08-03 17:11:43 +0200

    Client view (Orders) + Added client logic

--- client/app/orders/orders.controller.js
+++ client/app/orders/orders.controller.js
@@ -1,18 +1,47 @@
 'use strict';
 
-(function(){
+(function () {
+    class OrdersComponent {
+        constructor($http, $scope, socket) {
+            this.apiUrl = '/api/orderss/';
 
-class OrdersComponent {
-  constructor() {
-    this.message = 'Hello';
-  }
-}
+            this.$http = $http;
+            this.socket = socket;
 
-angular.module('companyFoodDeliveryApp')
-  .component('orders', {
-    templateUrl: 'app/orders/orders.html',
-    controller: OrdersComponent,
-    controllerAs: 'ordersCtrl'
-  });
+            this.orders = [];
+
+            $scope.$on('$destroy', function () {
+                socket.unsyncUpdates('orders')
+            });
+        }
+        
+        $onInit() {
+            this.$http.get(this.apiUrl)
+                .then(response => {
+                   this.orders = response.data;
+                   this.socket.syncUpdates(this.apiUrl, this.orders);
+                });
+        }
+        
+        addOrder() {
+            if(this.newOrder) {
+                this.$http.post(this.apiUrl, {
+                    
+                });
+                this.newOrder = '';
+            }
+        }
+
+        deleteOrder(order) {
+            this.$http.delete(this.apiUrl + order._id);
+        }
+    }
+
+    angular.module('companyFoodDeliveryApp')
+        .component('orders', {
+            templateUrl: 'app/orders/orders.html',
+            controller: OrdersComponent,
+            controllerAs: 'ordersCtrl'
+        });
 
 })();
